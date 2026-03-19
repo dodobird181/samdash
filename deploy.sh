501d7a5f6c6c12ea3024a8be9c99c9c76123119b
Sam <dodobird181@gmail.com>
2026-03-18 20:31:29 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,13 @@
 set -euo pipefail
 
 USER="dodob"
+
+echo "Pulling new code..."
 git pull origin main
+
+echo "Restarting service..."
 sudo -u $USER XDG_RUNTIME_DIR=/run/user/$(id -u $USER) systemctl --user restart samdash
 sleep 2  # in case service starts then crashes quickly
+
+echo "Verifying it restarted alright..."
 sudo -u $USER XDG_RUNTIME_DIR=/run/user/$(id -u $USER) systemctl --user is-active samdash
